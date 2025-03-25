2c82edf567e58f0d91489489f5db39584627ca90
신지원 <jwshin@ship-da.com>
2025-03-25 10:39:47 +0900

    [refactor] swaggerConfig 추가

---
+++ src/main/java/org/example/util/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package org.example.util;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openApi() {
+        return new OpenAPI().components(new Components()).info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info().title("Book App API")
+                .description("도서 목표 달성 앱")
+                .version("1.0.0");
+    }
+}
